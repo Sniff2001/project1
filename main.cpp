e6fb0c2f0e60235ed0123264598f7cdd9fc719d8
Flabergasted <karlelwynlokenbrown@gmail.com>
2023-09-01 16:07:48 +0200

    Added run task 7 to the three files

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 int main() {
     runTask2();
+    //runTask7();
+    
     // All is well. Exit program with return code 0.
     return 0;
 }
---
+++ task7.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+//#include <fstream>
+//#include <cmath>
+#include <vector>
+
+// Define the matrix
+int arr[4][4] = {
+	2,-1,0,0, -1,2,-1,0, 0,-1,2,-1, 0,0,-1,2
+}
+
+int runTask7() {
+	//gaussian elim here
+	
+	// - back substitution
+	
+	// - forward substitution
+
+
+	//return v and corelate with x
+	
+	//include boundary points
+	
+	//write to data file
+
+
+	//nsteps, descretization steps along full x axis
+	
+	//n_steps = 10, 100, 1000, ...
+
+
+	return 0;
+}
