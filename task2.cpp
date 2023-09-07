b72a1e9cd90e18b7a269cc098ede412483554308
Flabergasted <karlelwynlokenbrown@gmail.com>
2023-09-07 15:43:23 +0200

    Changed x definition from iota to for loop

--- task2.cpp
+++ task2.cpp
@@ -9,7 +9,7 @@
 std::vector<double> u(std::vector<double> x) {
     std::vector<double> new_vec(x.size());
     for (int i = 0; i < x.size(); i++) {
-        new_vec[i] = 1 - (1 - exp(-10)) * x[i] - exp(-10 * x[i]);
+        new_vec[i] = 1. - (1. - exp(-10.)) * x[i] - exp(-10. * x[i]);
     }
     return new_vec;
 }
@@ -27,17 +27,20 @@ int runTask2() {
     double x_min = 0.0;
     double x_max = 1.0;
     // Number of steps
-    int n_steps = 10;
+    int n_steps = 101;
+    double h = x_max / (n_steps-1);
 
     // Initialising the vector
     // First declare the size of the vector to be n_steps
-    std::vector<double> v1(n_steps);
-    // iota lets us make a vector with consecutive numbers
-    std::iota(v1.begin(), v1.end(), 1);
+    std::vector<double> x(n_steps);
+    for (int i = 0; i < n_steps; i++) 
+    {
+        x[i] = x[0] + h * i;
+    }
 
     // Print the vector to make sure the next for loop won't run into problems
     for (int i = 0; i < n_steps; i++) {
-        std::cout << v1.at(i) << " ";
+        std::cout << x.at(i) << " ";
     }
 
     // Some width and precision parameters we will use to format the output
@@ -45,13 +48,13 @@ int runTask2() {
     int prec = 4;
 
     // Initialize the values of u(x) using v1, which is a vector of x-values
-    std::vector<double> y = u(v1);
+    std::vector<double> y = u(x);
 
     // Loop over steps
     for (int i = 0; i < n_steps; i++)
     {
         // Write a line with the current x and y values (nicely formatted) to file
-        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << v1[i]
+        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x[i]
             << std::setw(width) << std::setprecision(prec) << std::scientific << y[i]
             << std::endl;
     }
